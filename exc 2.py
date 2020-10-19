825ab8487476ecea09a921250cb695a41155560f
BOT-Samuel <56443581+BOT-Samuel@users.noreply.github.com>
2020-10-19 14:39:26 -0300

    Update exc 2.py

--- exc 2.py
+++ exc 2.py
@@ -7,11 +7,11 @@ Somar todos os números.
 #Resolução do exerc 2 por BOT-Samuel
 
 lista2 = []
+t = 0
 n = 0
-
 for i in range(8):
-    x = int(input("Digite os valores inteiros:"))
-    lista2.append(x)
+    y = int(input("Digite um valor inteiro:"))
+    lista2.append(y)
 
 lista2.sort()
 print(lista2[:])
@@ -19,4 +19,5 @@ print(lista2[:])
 for c in range(8):
     if (lista2[c]>30):
         n = n + 1
-print ("Há {0} números maiores que 30.".format(n))
+    t = t + lista2[c]
+print ("Há {0} números maiores que 30 e a soma total dos nºs é {1}.".format(n,t))
